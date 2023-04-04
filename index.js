b70a1f2b58262255119a10a11a594ca4e2df9113
Kenneth <nitramnek@gmail.com>
2023-04-04 13:58:12 +0300

    Assignment done

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const currentUser = 'ken';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
 const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
 const shortGreeting = `Welcome, ${currentUser[0]} !`;
+
