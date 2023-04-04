e542d12c79943a5ec8414b60e182ebb80385a030
Kenneth <nitramnek@gmail.com>
2023-04-04 13:54:47 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+//k3n5c0d3
 // Write your code in this file!
+const currentUser = 'ken';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]} !`;
